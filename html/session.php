67f07378cf47177cbd3e7ccecc82327f2e0371db
hibagus <git@bagus.my.id>
2017-10-21 13:59:22 +0700

    Change Query

--- html/session.php
+++ html/session.php
@@ -4,7 +4,7 @@
    
    $user_check = $_SESSION['login_user'];
    
-   $ses_sql = mysqli_query($mysqli,"select username from admin where username = '$user_check' ");
+   $ses_sql = mysqli_query($mysqli,"select username from users where username = '$user_check' ");
    
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
    
