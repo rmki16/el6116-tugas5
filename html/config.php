531f91c2dbe6324e662e074fed0b974ea38bf108
hibagus <git@bagus.my.id>
2017-10-21 12:24:07 +0700

    Initial Commit

---
+++ html/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$databaseHost = 'el6116-mysql-server.vm';
+$databaseName = 'tugas5a';
+$databaseUsername = 'tugas5a';
+$databasePassword = 'tugas5a';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+?>
