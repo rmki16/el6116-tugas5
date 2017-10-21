97ae639f63fe9254e7ac66206be5a58abedf5ecb
hibagus <git@bagus.my.id>
2017-10-21 14:42:34 +0700

    Edit method to update the database

--- html/update.php
+++ html/update.php
@@ -5,10 +5,21 @@
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
       
-      $myusername = mysqli_real_escape_string($mysqli,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($mysqli,$_POST['password']); 
+	  $id = $_POST['id'];
+      $username = $_POST['username'];
+	  $firstname = $_POST['firstname'];
+	  $lastname = $_POST['lastname'];
+	  $pass_plain = $_POST['newpassword'];
+	  
+      $pass_hash = md5($pass_plain);
+      $sql = "UPDATE users SET username='$username', firstname='$firstname', lastname='$lastname', pass_plain = '$pass_plain', pass_hash = '$pass_hash' WHERE id=$id";
+      $result = mysqli_query($mysqli,$sql);
       
-      $sql = "SELECT id FROM users WHERE username = '$myusername' and pass_plain = '$mypassword'";
+	  
+	  // Relogin
+	  session_destroy();
+	  session_start();
+	  $sql = "SELECT id FROM users WHERE username = '$username' and pass_plain = '$pass_plain'";
       $result = mysqli_query($mysqli,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
@@ -18,7 +29,7 @@
       // If result matched $myusername and $mypassword, table row must be 1 row
 		
       if($count == 1) {
-         $_SESSION['login_user'] = $myusername;
+         $_SESSION['login_user'] = $username;
          
          header("location: welcome.php");
       }else {
@@ -34,7 +45,7 @@ echo $login_session;
 $result = mysqli_query($mysqli, "SELECT * FROM users WHERE username='$login_session'");
 while($res = mysqli_fetch_array($result))
 {
-	
+	$id		  = $res['id'];
 	$username = $res['username'];
 	$firstname = $res['firstname'];
 	$lastname = $res['lastname'];
@@ -79,6 +90,7 @@ while($res = mysqli_fetch_array($result))
             <div style = "margin:30px">
                
                <form action = "" method = "post">
+				  <label>UserID  :</label><input type = "text" name = "id" class = "box" value="<?php echo $id;?>"/><br /><br />
                   <label>UserName  :</label><input type = "text" name = "username" class = "box" value="<?php echo $username;?>"/><br /><br />
                   <label>FirstName  :</label><input type = "text" name = "firstname" class = "box" value="<?php echo $firstname ;?>"/><br/><br />
 				  <label>LastName:</label><input type = "text" name = "lastname" class = "box" value="<?php echo $lastname;?>"/><br/><br />
