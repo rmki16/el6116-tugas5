a8328a56eaa6930d674710f1a4e8e929b9b8042c
hibagus <git@bagus.my.id>
2017-10-21 12:43:42 +0700

    Salah Variabel Database.... harusnya mysqli bukan db

--- html/login.php
+++ html/login.php
@@ -5,11 +5,11 @@
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
       
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      $myusername = mysqli_real_escape_string($mysqli,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($mysqli,$_POST['password']); 
       
       $sql = "SELECT id FROM users WHERE username = '$myusername' and pass_plain = '$mypassword'";
-      $result = mysqli_query($db,$sql);
+      $result = mysqli_query($mysqli,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
       
